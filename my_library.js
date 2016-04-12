28b0f29d5a3c2f9ee088fc6ea1b3163e80cffaa1
Kassim Ouattara <ouat.kassim@gmail.com>
2016-04-12 18:43:57 -0400

    updating

--- my_library.js
+++ my_library.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
+	
+$('.error').hide();
 
-	$('#header1').animate({fontSize: "150px",
-		width: "50%",
-		color:"red",
-		marginLeft: "3000px",
-		marginTop: "5000px",
-		opacity:"0"} , 10000);
-});
+$('#btnSubmit').click(function(event){
+   var dtVal=$('#txtDate').val();
+   var dtVal2=$('#password').val();
+   if(ValidateDate(dtVal))
+   {
+      $('.error').hide();
+   }
+   else
+   {
+     $('.error').show();
+     event.preventDefault();
+   }
+   if(ValidatePassword(dtVal2))
+   {
+      $('.error2').hide();
+   }
+   else
+   {
+     $('.error2').show();
+       
+}
+
+
+
+
+
+   /////////////////
+
+	function ValidateDate(dtValue)
+	{
+	var dtRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
+	return dtRegex.test(dtValue);
+	}
+
+	function validatePassword(x){
+		if (x == "12345") {
+			console.log("Invalid Password. The password should be 12345!");
+		}else {
+			console.log("try again");
+		}
+	};
+
+   });
