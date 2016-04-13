d587030ad8aeadbc9c9f2c9074306b23b159ab80
Kassim Ouattara <ouat.kassim@gmail.com>
2016-04-13 11:39:50 -0400

    Adding username and password

--- my_library.js
+++ my_library.js
@@ -1,47 +1,46 @@
-$(document).ready(function(){
-	
-$('.error').hide();
+$(document).ready(function() {
+  $('.error').hide();
+  $('.error2').hide();
+  // $('#fname').keypress(function(event){
+    userCheck(event);
+  // });
 
-$('#btnSubmit').click(function(event){
-   var dtVal=$('#txtDate').val();
-   var dtVal2=$('#password').val();
-   if(ValidateDate(dtVal))
-   {
-      $('.error').hide();
-   }
-   else
-   {
-     $('.error').show();
-     event.preventDefault();
-   }
-   if(ValidatePassword(dtVal2))
-   {
-      $('.error2').hide();
-   }
-   else
-   {
-     $('.error2').show();
-       
-}
+  $('#btnSubmit').click(function(event){
+
+     var dtVal=$('#txtDate').val();
+     var dtVal2=$('#password').val();
+     console.log(dtVal2);
+     ValidateDate(dtVal)
+     if(ValidateDate(dtVal)) {
+        $('.error').hide();
+     } else {
+       $('.error').show();
+       event.preventDefault();
+     }
+     var dtVal3 = $('#fname').val();
+     validatePassword(dtVal2);
+     userCheck(dtVal3);
+  });
 
 
+function ValidateDate(dtValue) {
+  var dtRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
+  return dtRegex.test(dtValue);
+};
 
+function validatePassword(x){
+    if (x != '12345') { 
+      $('.error2').show();
+    }
+  };
 
+function userCheck(){
+       dtVal3 = $('#fname').val();
+      if(dtVal3 == ""){
+        $('.error3').show();
+      } else {
+         $('.error3').fadeOut(2000);
+      }
+    }
+  });  
 
-   /////////////////
-
-	function ValidateDate(dtValue)
-	{
-	var dtRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
-	return dtRegex.test(dtValue);
-	}
-
-	function validatePassword(x){
-		if (x == "12345") {
-			console.log("Invalid Password. The password should be 12345!");
-		}else {
-			console.log("try again");
-		}
-	};
-
-   });
